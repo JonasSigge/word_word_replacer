d6c084277a089e0996f0f3232c909d849194048f
unknown <jonas.gustafsson@MITAUTO,local>
2018-06-12 15:22:06 +0200

    Added helpers.py file

--- main.py
+++ main.py
@@ -2,3 +2,30 @@
 """
 
 
+import re
+
+from zipfile import ZipFile
+
+
+inputZipFile = ZipFile('testin.docx')
+outputZipFile = ZipFile('testout.docx',"a")
+macros = {'(%pris%)':'500','(%namn%)':'Jonas'}
+
+with inputZipFile.open('word/document.xml') as zippedXMLText:
+    XMLtext = str(zippedXMLText.read())
+        
+for item, key in macros.items():
+    XMLtext = re.sub(key,item,XMLtext)
+
+
+for file in inputZipFile.filelist:
+    if not file.filename == "word/document.xml":
+        outputZipFile.writestr(file.filename, XMLtext)
+    else:
+        outputZipFile.writestr(file.filename, inputZipFile.read(file))
+    
+
+inputZipFile.close()
+outputZipFile.close()
+
+
